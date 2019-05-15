df21a34391bb4f47a8ad6922609843dfa1b08c6e
Payton Lower <paytown@me.com>
2019-05-15 15:19:39 -0700

    font selector added

--- src/components/TextFormatter.js
+++ src/components/TextFormatter.js
@@ -2,8 +2,9 @@ import React, { PureComponent } from 'react';
 
 export default class TextFormatter extends PureComponent {
   state = {
-    text: '',
+    text: 'Chose your favorite font and color ;)',
     formattedText: '',
+    font: 'Comic Sans MS',
     color: '#ff0000'
   }
 
@@ -14,14 +15,20 @@ export default class TextFormatter extends PureComponent {
   render() {
     const {
       text,
+      font,
       color
     } = this.state;
 
+    const fontOptions = ['Comic Sans MS', 'Papyrus'].map(font => {
+      return <option key={font} value={font}>{font}</option>;
+    });
+
     return (
       <>
+      <select name="font" value={font} onChange={this.handleChange}>{fontOptions}</select>
       <input type="text" name="text" value={text} onChange={this.handleChange} />
       <input type="color" name="color" value={color} onChange={this.handleChange} />
-      <pre style={{ color }}>{text}</pre>
+      <pre style={{ color: color, fontFamily: font }}>{text}</pre>
       </>
     );
   }
