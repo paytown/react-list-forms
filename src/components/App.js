d2c174f883278eef345d665507ed233cfae791cd
Payton Lower <paytown@me.com>
2019-05-15 17:31:45 -0700

    clickable

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from './Sidebar';
 
 export default function App(){
+  const [selected, updateSelected] = useState('home');
+
+  const content = {
+    home: 'Welcome back',
+    prices: '$400.000',
+    contact: 'papajohns@pizzahut.gov'
+  };
+
   return (
-    <Sidebar>
-      <p>hi1</p>
-      <p>hi2</p>
-      <p>hi3</p>
-    </Sidebar>
+    <div>
+      <Sidebar select={updateSelected}>
+        <a onClick={() => updateSelected('home')} href="#">home</a>
+        <a onClick={() => updateSelected('prices')} href="#">prices</a>
+        <a onClick={() => updateSelected('contact')} href="#">contact</a>
+      </Sidebar>
+      <p>{content[selected]}</p>
+    </div>
   );
 }
