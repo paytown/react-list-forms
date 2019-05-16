b33e48eae751b56629d2aa7cf8ca90bcecc047ce
Payton Lower <paytown@me.com>
2019-05-15 17:26:06 -0700

    baseline

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+
+export default function App(){
+  return (
+    <Sidebar>
+      <p>hi1</p>
+      <p>hi2</p>
+      <p>hi3</p>
+    </Sidebar>
+  );
+}
---
+++ src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Sidebar({ children }){
+  return (
+    <nav>
+      {children}
+    </nav>
+  );
+}
+
+Sidebar.propTypes = {
+  children: PropTypes.node
+};
+
+export default Sidebar;
